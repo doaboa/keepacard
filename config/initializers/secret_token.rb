6b1f6d735188c02a82740e9a6d25bb31e7d32a06
MakerSquare Student <student@lab-7.att.net>
2013-06-28 13:54:52 -0500

    intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Keepacard::Application.config.secret_token = '7ebad4a4333a6cc03c72d1493354b989dfd630316dd5c42ea9d62de8f0267cb4452d98c0c82e5c5a00159125b56b6464a106f5e5e0fa5d2973ed3d1fb580757b'
